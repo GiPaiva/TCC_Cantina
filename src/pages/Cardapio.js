a4beb9a06582e9c038bda133f4b722dfc971afa5
GiPaiva <gi.paiva.2005@gmail.com>
2023-03-08 10:21:46 -0300

    produtos

--- src/pages/Cardapio.js
+++ src/pages/Cardapio.js
@@ -12,24 +12,138 @@ function Cardapio() {
         <div className='card'>
           <div className='esquerda'>
             <div>
-              <p className='salgado'>Salgados</p>
+              <p className='produto'>Salgados</p>
             </div>
-            <div className="header-detalhesC">
-              <div className="header-detalhes-div1"></div>
-              <div className="header-detalhes-div2"></div>
-              <div className="header-detalhes-div3"></div>
+            <div className="body-detalhes">
+              <div className="body-detalhes-div1"></div>
+              <div className="body-detalhes-div2"></div>
+              <div className="body-detalhes-div3"></div>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
             </div>
           </div>
-          {/* <div className='direita'></div>
-          <div className='Lanches'></div>
-          <div className='Pasteis'></div>
-          <div className='Tapioca-Salgada'></div>
-          <div className='Tapioca-Doce'></div>
-          <div className='Sucos'></div>
-          <div className='Refri'></div>
-          <div className='Pizza'></div>
-          <div className='Sobremesa'></div>
-          <div className='Açaí'></div> */}
+
+          <div className='direita'>
+            <div className="body-detalhesD">
+              <div className="body-detalhes-div2"></div>
+              <div className="body-detalhes-div3"></div>
+              <div className="body-detalhes-div1"></div>
+            </div>
+            <div>
+              <p className='produto'>Lanches</p>
+            </div>
+          </div>
+
+          <div className='esquerda'>
+            <div>
+              <p className='produto'>Pasteis</p>
+            </div>
+            <div className="body-detalhes">
+              <div className="body-detalhes-div3"></div>
+              <div className="body-detalhes-div2"></div>
+              <div className="body-detalhes-div1"></div>
+            </div>
+            <div className='lista-produto'>
+              <ul>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='direita'>
+            <div className="body-detalhesD">
+              <div className="body-detalhes-div3"></div>
+              <div className="body-detalhes-div1"></div>
+              <div className="body-detalhes-div2"></div>
+            </div>
+            <div>
+              <p className='produto'>Tapioca-Salgada</p>
+            </div>
+          </div>
+
+          <div className='esquerda'>
+            <div>
+              <p className='produto'>Tapioca-Doce</p>
+            </div>
+            <div className="body-detalhes">
+              <div className="body-detalhes-div1"></div>
+              <div className="body-detalhes-div3"></div>
+              <div className="body-detalhes-div2"></div>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='direita'>
+            <div className="body-detalhesD">
+              <div className="body-detalhes-div2"></div>
+              <div className="body-detalhes-div1"></div>
+              <div className="body-detalhes-div3"></div>
+            </div>
+            <div>
+              <p className='produto'>Sucos</p>
+            </div>
+          </div>
+
+          <div className='esquerda'>
+            <div>
+              <p className='produto'>Refri</p>
+            </div>
+            <div className="body-detalhes">
+              <div className="body-detalhes-div3"></div>
+              <div className="body-detalhes-div1"></div>
+              <div className="body-detalhes-div2"></div>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='direita'>
+            <div className="body-detalhesD">
+              <div className="body-detalhes-div2"></div>
+              <div className="body-detalhes-div3"></div>
+              <div className="body-detalhes-div1"></div>
+            </div>
+            <div>
+              <p className='produto'>Pizza</p>
+            </div>
+          </div>
+
+          <div className='esquerda'>
+            <div>
+              <p className='produto'>Sobremesa</p>
+            </div>
+            <div className="body-detalhes">
+              <div className="body-detalhes-div2"></div>
+              <div className="body-detalhes-div1"></div>
+              <div className="body-detalhes-div3"></div>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
+            </div>
+          </div>
+
+          <div className='direita'>
+            <div className="body-detalhesD">
+              <div className="body-detalhes-div1"></div>
+              <div className="body-detalhes-div3"></div>
+              <div className="body-detalhes-div2"></div>
+            </div>
+            <div>
+              <p className='produto'>Açaí</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
