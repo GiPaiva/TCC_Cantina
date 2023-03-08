ed357dc431c428e7a1b4dfb46b9889485a9ce018
GiPaiva <gi.paiva.2005@gmail.com>
2023-03-08 10:54:43 -0300

    links

--- src/pages/Cardapio.js
+++ src/pages/Cardapio.js
@@ -8,11 +8,21 @@ function Cardapio() {
         <Header></Header>
       </div>
       <div className='container'>
-        {/* <div className='links'></div> */}
+        <div className='caixa links'>
+          <a class="page" href="#salgado">Salgado</a>
+					<a class="page" href="#lanches">Lanches</a>
+					<a class="page" href="#pasteis">Pasteis</a>
+					<a class="page" href="#tapioca-salgada">Tapioca-Salgada</a>
+					<a class="page" href="#tapioca-doce">Tapioca-Doce</a>
+					<a class="page" href="#sucos">Sucos</a>
+					<a class="page" href="#refris">Refris</a>
+					<a class="page" href="#sobremesa">Sobremesa</a>
+					<a class="page" href="#acai">Açaí</a>
+        </div>
         <div className='card'>
           <div className='esquerda'>
             <div>
-              <p className='produto'>Salgados</p>
+              <p className='produto' id='salgado'>Salgados</p>
             </div>
             <div className="body-detalhes">
               <div className="body-detalhes-div1"></div>
@@ -33,13 +43,18 @@ function Cardapio() {
               <div className="body-detalhes-div1"></div>
             </div>
             <div>
-              <p className='produto'>Lanches</p>
+              <p className='produto' id='lanches'>Lanches</p>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
             </div>
           </div>
 
           <div className='esquerda'>
             <div>
-              <p className='produto'>Pasteis</p>
+              <p className='produto' id='pasteis'>Pasteis</p>
             </div>
             <div className="body-detalhes">
               <div className="body-detalhes-div3"></div>
@@ -60,13 +75,18 @@ function Cardapio() {
               <div className="body-detalhes-div2"></div>
             </div>
             <div>
-              <p className='produto'>Tapioca-Salgada</p>
+              <p className='produto' id='tapioca-salgada'>Tapioca-Salgada</p>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
             </div>
           </div>
 
           <div className='esquerda'>
             <div>
-              <p className='produto'>Tapioca-Doce</p>
+              <p className='produto' id='tapioca-doce'>Tapioca-Doce</p>
             </div>
             <div className="body-detalhes">
               <div className="body-detalhes-div1"></div>
@@ -87,13 +107,18 @@ function Cardapio() {
               <div className="body-detalhes-div3"></div>
             </div>
             <div>
-              <p className='produto'>Sucos</p>
+              <p className='produto' id='sucos'>Sucos</p>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
             </div>
           </div>
 
           <div className='esquerda'>
             <div>
-              <p className='produto'>Refri</p>
+              <p className='produto' id='refri'>Refri</p>
             </div>
             <div className="body-detalhes">
               <div className="body-detalhes-div3"></div>
@@ -114,13 +139,18 @@ function Cardapio() {
               <div className="body-detalhes-div1"></div>
             </div>
             <div>
-              <p className='produto'>Pizza</p>
+              <p className='produto' id='pizza'>Pizza</p>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
             </div>
           </div>
 
           <div className='esquerda'>
             <div>
-              <p className='produto'>Sobremesa</p>
+              <p className='produto' id='sobremesas'>Sobremesa</p>
             </div>
             <div className="body-detalhes">
               <div className="body-detalhes-div2"></div>
@@ -141,10 +171,16 @@ function Cardapio() {
               <div className="body-detalhes-div2"></div>
             </div>
             <div>
-              <p className='produto'>Açaí</p>
+              <p className='produto' id='acai'>Açaí</p>
+            </div>
+            <div>
+              <ul>
+                <li></li>
+              </ul>
             </div>
           </div>
         </div>
+        <div className='caixa'></div>
       </div>
     </div>
     );
