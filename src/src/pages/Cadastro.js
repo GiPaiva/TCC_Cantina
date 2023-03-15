bf9d033544b89c323ee569832ae354e996b19d7a
HLN-lobo <98746888+HLN-lobo@users.noreply.github.com>
2023-03-15 11:35:34 -0300

    responsividade reset

---
+++ src/src/pages/Cadastro.js
@@ -0,0 +1,41 @@
+import Header from "../ui/components/Header";
+import "../ui/styles/Cadastro.css";
+
+function Cadastro() {
+	return (
+		<div>
+			<div>
+				<Header></Header>
+			</div>
+			<div className="container">
+                <div className="cadastro">
+                    <h1>Cadastro de Cliente </h1>
+                </div>
+				<div className="caixa"></div>
+				<div className="card-login">
+					<div id="login">
+						<form className="form">
+							<div className="field" style={{ color: "black" }}>
+								<label htmlFor="usuario">Nome</label>
+								<input type="usuario" name="usuario" id="usuario" />
+							</div>
+							<div className="field" style={{ color: "black" }}>
+								<label htmlFor="password">Telefone</label>
+								<input type="telefone" name="telefone" id="password" />
+							</div>
+
+							<div className="actions">
+								<button type="submit" id="button">
+									Salvar
+								</button>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div className="caixa"></div>
+			</div>
+		</div>
+	);
+}
+
+export default Cadastro;
