c8e29ab1ffa27fc1335553fa443b805c7358937b
Gustavo Lima <105643784+gustavol1@users.noreply.github.com>
2023-03-17 11:48:08 -0300

    Update Cadastro.js

--- src/pages/Cadastro/Cadastro.js
+++ src/pages/Cadastro/Cadastro.js
@@ -11,20 +11,24 @@ function Cadastro(){
             <div className='container'>
                 <div className="caixa"></div>
                 <div className="card cadastro">
-                    <div className="title1">
-                            <h1 className="title">Cadastro de Cliente</h1>
+                    <div className="title1" id="title">
+                            <h1 className="title">Cadastro de<br></br> Cliente</h1>
+                            <br></br>
+                            <br></br>
                         <div className="formulario">
                             <form action="" className="form-cadastro">
                             <input className="inputt" type="text" name="username" id="username" placeholder="Nome"/>
                             <input className="inputt" type="telefone" name="telefone" id="telefone" placeholder="Telefone"/>
-                            <button className="btn-cadastro" type="submit">Salvar</button>
                             </form>
                         </div>
+                        <br/>
+                        <button className="btn-cadastro" type="submit">Salvar</button>
                     </div>
                 </div>
                 <div className="caixa"></div>
             </div>
         </div>
+
     )
 }
 
