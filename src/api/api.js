663d962353854b03d9f7e5b0e90ba1ee914867cc
GiPaiva <gi.paiva.2005@gmail.com>
2023-04-14 13:55:23 -0300

    Update api.js
    Todas as rotas estão criadas para a ligação com o back

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,8 @@
 import axios from '.';
 
+
+
+//Rotas protegidas com key
 export const getProducts = async () => {
   try {
     const response = await axios.get(`/produtos`, {
@@ -27,6 +30,15 @@ export const getProductsByCategory = async (categoria) => {
   }
 };
 
+
+
+
+
+
+
+//Rotas protegidas por Token
+//Rotas Usuarios
+//Rota de login
 export const login = async (userName, senha) => {
   try {
     const response = await axios.post(`/usuarios`, {
@@ -38,10 +50,36 @@ export const login = async (userName, senha) => {
     console.error(error);
   }
 };
+//Rota de cadastrar usuario
+export const cadastraFun = async (userName, senha, confirmaSenha) => {
+  try {
+    const response = await axios.post(`/usuarios/cadastro`, {
+      userName,
+      senha,
+      confirmaSenha
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-export const editProduto = async (id, token) => {
+
+
+
+
+
+//Rotas Produtos
+// Rota de atualizar produtos
+export const EditProduto = async (id,nome, preco, categoria, descricao, token) => {
   try{
-    const response = await axios.put(`/produtos/${id}`, {
+    const response = await axios.put(`/produtos/`, {
+      body: {
+        nome, preco, categoria, descricao,
+      },
+      params: {
+        id
+      },
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -53,10 +91,13 @@ export const editProduto = async (id, token) => {
   }
 }
 
-
-export const createProduct = async (productData, token) => {
+// Rota de adicionar produtos
+export const AddProduct = async (nome, preco, categoria, descricao, token) => {
   try {
-    const response = await axios.post(`/produtos`, productData, {
+    const response = await axios.post(`/produtos`, {
+      body: {
+        nome, preco, categoria, descricao,
+      },
       headers: {
         Authorization: `Bearer ${token}`,
       },
@@ -67,17 +108,220 @@ export const createProduct = async (productData, token) => {
   }
 };
 
+// Rota de deletar produtos
+export const DeleteProduto = async (id ,token) => {
+  try {
+    const response = await axios.post(`/produtos`, {
+      params: {
+        id
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
-// export const Auth = async (local,token) => {
-//   try {
-//     const response = await axios.post(`/`, {
-//       headers: {
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
 
+
+
+
+
+
+//Rotas Compras
+// Rota de pegar todas as compras
+export const Compras = async (token) => {
+  try {
+    const response = await axios.get(`/compras`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Rota para listar todas as compras de um cliente específico
+export const ClienteCompras = async (numero, token) => {
+  try {
+    const response = await axios.get(`/compras`, {
+      params: {
+        numero
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Rota de criar nova compra
+export const AddCompras = async (id_cliente, compra, total, dataHora, token) => {
+  try {
+    const response = await axios.post(`/compras`, {
+      body: {
+        id_cliente, compra, total, dataHora
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Rota de alterar a compra
+export const EditCompra = async (id, id_cliente, compra, total, dataHora, numero, token) => {
+  try {
+    const response = await axios.put(`/compras`, {
+      body: {
+        id_cliente, compra, total, dataHora, numero
+      },
+      params: {
+        id
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Rota de excluir a compra
+export const deleteCompra = async (id, token) => {
+  try {
+    const response = await axios.delete(`/compras`, {
+      params: {
+        id
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// Excluir todas as compras de um único cliente
+export const AllDeleteCompraCliente = async () => {
+  try {
+    const response = await axios.delete(`/compras/cliente/`, {
+      params: {
+        numero
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+
+
+
+
+//Rotas Clientes
+//Rota para cadastrar um cliente
+export const CadastraClientes = async (nome, numero,token) => {
+  try {
+    const response = await axios.post(`/clientes`, {
+      body: {
+        nome, numero,
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//Rota para listar todos os clientes
+export const GetCliente = async (token) => {
+  try {
+    const response = await axios.get(`/clientes`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//Rota para listar cliente específico 
+export const ClienteId = async (id, token) => {
+  try {
+    const response = await axios.get(`/clientes`, {
+      params: {
+        id
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+//Rota para atualizar um cliente específico
+export const compras = async (id, nome, numero, token) => {
+  try {
+    const response = await axios.put(`/clientes`, {
+      body: {
+        nome, numero,
+      },
+      params: {
+        id
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+//Rota para excluir um cliente específico
+export const DeleteCliente = async (id ,token) => {
+  try {
+    const response = await axios.post(`/clientes`, {
+      params: {
+        id
+      },
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
