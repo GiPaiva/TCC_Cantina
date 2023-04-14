739a510e491c6772908f3bafb7a6431d9eab26ea
Gustavo Lima <105643784+gustavol1@users.noreply.github.com>
2023-04-14 11:32:48 -0300

    Add files via upload

---
+++ src/pages/CadastroFunc/Funcionario.js
@@ -0,0 +1,33 @@
+import '../../ui/styles/Cadastro.css';
+
+function Funcionario(){
+    return (
+        <div>
+            {/* Fiz algumas modificações apenas por conta que estava conflitando com as outras paginas */}
+            <div className='container'>
+                <div className="caixa"></div>
+                <div className="card cadastro">
+                    <div className="title1" id="">
+                            <h1 className="title">Cadastro de Funcionário</h1>
+                            <br></br>
+                            <br></br>
+                        <div className="formulario">
+                            {/* Formulário onde há a inserção das informações pelos inputs */}
+                            <form action="" className="form-cadastro">
+                            {/* Inputs e botão que usamos para fazer a inserção e entrega das informações pelos campos respectivos */}    
+                            <input className="inputt" type="text" name="username" id="username" placeholder="Nome"/>
+                            <input className="inputt" type="telefone" name="telefone" id="telefone" placeholder="Telefone"/>
+                            </form>
+                        </div>
+                        <br/>
+                        <button className="btn-cadastro" type="submit">Salvar</button>
+                    </div>
+                </div>
+                <div className="caixa"></div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Funcionario;
